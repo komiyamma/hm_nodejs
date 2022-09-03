636ca6ed18fbe99f92d667796d6d7cf935428e6d
komiyamma <komiyama0629@gmail.com>
2022-09-04 03:35:38 +0900

    ファイルサイズが200kほど小さくなる次期バージョン候補

--- hmEdgeJS.src/hmEdgeJSStaticLib/HmOriginalEncodeMap.cs
+++ hmEdgeJS.src/hmEdgeJSStaticLib/HmOriginalEncodeMap.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/*
 public sealed partial class hmEdgeJSDynamicLib
 {
     public sealed partial class HmOriginalEncodeMap
@@ -65552,3 +65552,4 @@ public sealed partial class hmEdgeJSDynamicLib
     }
     
 }
+*/
