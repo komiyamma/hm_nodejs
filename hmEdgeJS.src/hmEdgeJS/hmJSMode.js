8365f5436635519c5deea4c753386868d07b093c
komiyamma <komiyama0629@gmail.com>
2022-09-09 22:23:14 +0900

    releaseobjectの型ミス

--- hmEdgeJS.src/hmEdgeJS/hmJSMode.js
+++ hmEdgeJS.src/hmEdgeJS/hmJSMode.js
@@ -815,7 +815,7 @@
  hc.keepdll = function(){var m="keepdll";eval(st);return r;}
 
  hc.createobject = function(){var m="createobject";eval(fn); return r;}
- hc.releaseobject = function(){var m="releaseobject";eval(fn); return r;}
+ hc.releaseobject = function(){var m="releaseobject";eval(st); return r;}
  hc.member = {
     rnum:function(){var m="member";eval(fn); return r;},
     rstr:function(){var m="member";eval(fs); return r;}
